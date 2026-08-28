0ba45c40dc8aabed8af939441bdfc67c4e37e696
siddarth24 <siddarthanke@gmail.com>
2026-08-28 07:22:52 +0530

    contreoller class added and return to srg.html

--- src/main/java/com/srg/demo/contoller/HomeController.java
+++ src/main/java/com/srg/demo/contoller/HomeController.java
@@ -1,14 +1,15 @@
 package com.srg.demo.contoller;
 
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+
+@Controller
 public class HomeController {
 
     @GetMapping("/")
     public String home() {
-        return "Chat SRG API is running successfully!";
+        return "forward:/srg.html";
     }
 }
